805253fc2a04f63121495558526b7136421dd2dc
JaaferJaafer4 <jjaafar006@gmail.com>
2023-11-06 18:20:55 +0100

    Cross origin fix

--- src/main/java/tn/esprit/spring/kaddem/config/CorsConfig.java
+++ src/main/java/tn/esprit/spring/kaddem/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://192.168.56.20:80")
+                        .allowedOrigins("http://192.168.56.20:8089")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
